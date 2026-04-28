7cbb3f5345d065e35676069f1c73cf3e77216306
Tanamao Plus <tanamao.plus.app@gmail.com>
2026-04-28 08:46:55 -0400

    fix agenda

--- routes/agendaRoutes.js
+++ routes/agendaRoutes.js
@@ -1,12 +1,25 @@
-const router = require('express').Router();
-const controller = require('../controllers/agendaController');
-const { verifyToken } = require('../middleware/verifyToken');
+const express = require('express');
+const router = express.Router();
 
+const controller = require('../controllers/agendaController');
+const verifyToken = require('../middleware/verifyToken');
+
+// CRIAR AGENDAMENTO
 router.post('/', verifyToken, controller.criar);
+
+// LISTAR DO PROFISSIONAL
 router.get('/me', verifyToken, controller.listar);
+
+// LISTAR DO CLIENTE
 router.get('/cliente', verifyToken, controller.listarCliente);
 
+// 🔥 ABRIR CHAT PELO AGENDAMENTO
 router.post('/:id/chat', verifyToken, controller.abrirChatCliente);
 
+// EDITAR
 router.put('/:id', verifyToken, controller.editar);
+
+// CANCELAR
 router.delete('/:id', verifyToken, controller.cancelar);
+
+module.exports = router;
