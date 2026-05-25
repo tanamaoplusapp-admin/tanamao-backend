b47a539b6df2045f8ee07243566df57cec06e8ea
Tanamao Plus <tanamao.plus.app@gmail.com>
2026-05-25 13:47:20 -0400

    fix servicos

--- routes/mercadoPagoWebhookRoutes.js
+++ routes/mercadoPagoWebhookRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/mercadoPagoWebhookController');
 
-// Mercado Pago exige POST público
 router.post('/', controller.webhook);
+
 module.exports = router;
